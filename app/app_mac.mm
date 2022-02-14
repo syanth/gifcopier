60ca2af0e59d7d8708bd0b800ca7c1063571ae64
syanth <syanth@gmail.com>
2022-02-14 00:06:38 -0500

    Initial Mac Pasteboard Copy

---
+++ app/app_mac.mm
@@ -0,0 +1,14 @@
+#include <Appkit/Appkit.h>
+#include "clipapp.h"
+
+int CopyToClipboard(std::string pathStr) {
+    NSString *urlNSStr = [NSString stringWithUTF8String:pathStr.c_str()];
+    NSURL *URL = [NSURL URLWithString:urlNSStr];
+
+    NSPasteboard* pasteboard = [NSPasteboard generalPasteboard];
+    [pasteboard clearContents];
+    [pasteboard writeObjects:@[URL]];
+    [pasteboard setString:URL.absoluteString forType:NSPasteboardTypeFileURL];
+
+    return 0;
+}
