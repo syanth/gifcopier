76b35836a63cfe4dbb49e4252568c7c31c5a33b9
syanth <syanth@gmail.com>
2022-02-24 00:58:46 -0500

    Added test clip and path handling for it

--- app/app_test.cpp
+++ app/app_test.cpp
@@ -1,8 +1,19 @@
 #include "gtest/gtest.h"
 #include "clipapp.h"
+#include "Windows.h"
+#include <iostream>
 
 TEST(CopyToClipboardTest, CopyLocalClip) {
-	std::string pathStr = "C:\\dev\\test\\copperfield.mp4"; // Temp path for testing
-	//std::string pathStr = "file:///Users/syanth/Documents/jon.mp4";
+	WCHAR path[MAX_PATH];
+	GetModuleFileNameW(NULL, path, MAX_PATH);
+	std::wstring pathWstr(path);
+
+	size_t srcPath = pathWstr.find(L"build");
+	pathWstr.erase(srcPath);
+	pathWstr.append(L"tests\\copperfield.mp4");
+
+	std::string pathStr(pathWstr.begin(), pathWstr.end());
+	std::cout << pathStr;
+
 	EXPECT_EQ(CopyToClipboard(pathStr), 0);
 }
