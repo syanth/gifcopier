d5d0d6d3c8d5089f27d5b2235ee624b695712a32
syanth <syanth@gmail.com>
2022-02-28 22:15:04 -0500

    Updated test and workflows

--- app/app_test.cpp
+++ app/app_test.cpp
@@ -1,8 +1,6 @@
 #include "gtest/gtest.h"
 #include "clipapp.h"
 
-TEST(CopyToClipboardTest, CopyLocalClip) {
-	std::string pathStr = "C:\\dev\\test\\copperfield.mp4"; // Temp path for testing
-	//std::string pathStr = "file:///Users/syanth/Documents/jon.mp4";
-	EXPECT_EQ(CopyToClipboard(pathStr), 0);
+TEST(CopyToClipboardTest, CopyEmpty) {
+	EXPECT_EQ(CopyToClipboard(""), 0);
 }
