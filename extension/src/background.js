cd60348e9964706cd02910912fefbeca0fe25d71
syanth <syanth@gmail.com>
2022-03-15 22:44:48 -0400

    Implemented native messaging

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,21 +1,40 @@
 import browser from "webextension-polyfill"
 
+var port = browser.runtime.connectNative("com.syanth.clipcopier")
+
+function onResponse(response) {
+    console.log("Received " + JSON.stringify(response));
+}
+  
+function onError(error) {
+    console.log(`Error: ${error}`);
+}
+
 browser.contextMenus.create({
     id: "copy-gifv",
     title: "Copy GIFV",
     contexts: ["video"]
 });
 
-browser.contextMenus.create({
-    id: "paste-clip",
-    title: "Paste Clip",
-    contexts: ["editable"]
-});
-
 browser.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === "copy-gifv") {
-        console.log(info.srcUrl);
-        display(info.srcUrl);
+        console.log(info);
+        console.log("Original URL: ", info.srcUrl);
+        let fixedURL = getURL(info.srcUrl);
+        console.log("Fixed URL: ", fixedURL)
+        console.log("Sending URL");
+        var sending = browser.runtime.sendNativeMessage(
+          "com.syanth.clipcopier",
+          fixedURL);
+        sending.then(onResponse, onError);
     }
 }
 );
+
+function getURL(URL) {
+    if (URL.includes("preview.redd.it") && !URL.includes("external")) {
+        URL = URL.replace("preview", "i");
+        return URL;
+    }
+    return URL
+}
